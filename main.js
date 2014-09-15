2f885a865d1008b1f98329e5f37472cb16dde6e3
Adam Weiner <adamwineguy@gmail.com>
2014-09-15 13:58:21 -0600

    completed first exercise in js

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-var word1 = prompt("Please enter any word");
-alert("You entered" + " " + word1 );
-alert("The word you entered is " + word1.length + " letters long.")
+var word1 = prompt("Please enter any word. Any fucking word at all!");
+alert("You entered" + " " + '"' + word1 + '"'+ "." + " The word you entered is " +  word1.length + " letters long." + " The third letter in your word is " + '"' + word1.charAt(2) + '"' + "." + " This is your word in lowercase: " + '"' + word1.toLowerCase() + '"' + "." + " This is your word in uppercase: " + '"' + word1.toUpperCase() + '"' + "." + " This is your word in a sentence: " + " You love to eat " + '"' + word1 + '"' + "." + " These are second to fourth letters in your word: " + '"' + word1.substring(1, 4) + '"' + "." + " Take that beeyatch!");
